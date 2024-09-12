5db6a3f9aefa1280c84f25cc2e0ce3d4be81ed88
Mahil Anthony <mahilsamson2016@gmail.com>
2024-09-12 15:09:19 +0530

    Update EXP_13_080.cpp

--- EXP_13_080.cpp
+++ EXP_13_080.cpp
@@ -34,3 +34,48 @@ Addition of 3 integers: 6
 */
 
 //Program 2 (Operator Overloading)
+//Program 2 (Operator Overloading)
+#include<iostream>
+using namespace std;
+class Complex
+{
+    private:
+    float real;
+    float imag;
+    public:
+    Complex(float r = 0, float i = 0)
+    {
+        real = r;
+        imag = i;
+    }
+    Complex operator + (const Complex &obj)
+    {
+        Complex temp;
+        temp.real = real+obj.real;
+        temp.imag = imag+obj.imag;
+        return temp;
+    }
+    void display()const
+    {
+        cout<<real<<"+"<<imag<<"i"<<endl;
+    }
+};
+int main()
+{
+    Complex c1(3.5, 2.5);
+    Complex c2(1.6, 3.7);
+    Complex c3 = c1+c2;
+    cout<<"First complex number: ";
+    c1.display();
+    cout<<"Second complex number: ";
+    c2.display();
+    cout<<"Third complex number: ";
+    c3.display();
+}
+
+//Output
+/*
+First complex number: 3.5+2.5i
+Second complex number: 1.6+3.7i
+Third complex number: 5.1+6.2i
+*/
