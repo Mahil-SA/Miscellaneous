74a19c88bee577a71c361c9b9c243135c3bc2b6c
Mahil Anthony <mahilsamson2016@gmail.com>
2024-09-13 10:29:27 +0530

    Update EXP_15_080.cpp

--- EXP_15_080.cpp
+++ EXP_15_080.cpp
@@ -1,1 +1,32 @@
+//Experiment 15 - To study and implement about recursion.
+//Name - Mahil Anthony
+//PRN -  23070123080
+//Batch -  ENTC B1
 
+//Program 1 (Find factorial of number using recursion.)
+#include<iostream>
+using namespace std;
+int factorial (int n)
+    {
+        if(n==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n*factorial(n-1);
+        }
+    }
+int main()
+{
+    int n;
+    cout<<"Enter the number you want factorial of: ";
+    cin>>n;
+    cout<<"The factorial of "<<n<<" is "<<factorial(n)<<"."<<endl;
+}
+
+//Output
+/*
+Enter the number you want factorial of: 5
+The factorial of 5 is 120.
+*/
