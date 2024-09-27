88311ab1ac6a0848abea0f62ed6e52261b00b798
Mahil Anthony <mahilsamson2016@gmail.com>
2024-09-27 10:22:40 +0530

    Update EXP_16_080.cpp

--- EXP_16_080.cpp
+++ EXP_16_080.cpp
@@ -29,3 +29,31 @@
 Enter a positive number: -34
 You have entered -34 which is a negative number.
 */
+
+//Program 2 (Prompt the user to enter a year greater than 2000. If user enters a year greater than 2000, then display that year. If user enters year less than 2000 throw an exception.)
+ #include<iostream>
+ using namespace std;
+  int main(){
+     float year;
+      cout<<"Enter a year greater than 2000: ";
+      cin>>year;
+       
+      try
+      {
+          if(year<2000)
+          {
+              throw "You have entered a year less than 2000!";
+          }
+          cout<<"The entered year is: "<<year; 
+      }
+      catch(const char *msg)
+      {
+          cout<<msg;
+      }
+}
+
+//Output
+/*
+Enter a year greater than 2000: 1999
+You have entered a year less than 2000!
+*/
