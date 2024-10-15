06aa6587045963a3051edbfed2d525284c3cec68
Mahil Anthony <mahilsamson2016@gmail.com>
2024-10-15 17:41:05 +0530

    Update EXP_17_080.cpp

--- EXP_17_080.cpp
+++ EXP_17_080.cpp
@@ -1,4 +1,4 @@
-//Experiment 16 - To study and implement about exception handling.
+//Experiment 17 - To study and implement about linked lists.
 //Name - Mahil Anthony
 //PRN -  23070123080
 //Batch -  ENTC B1
