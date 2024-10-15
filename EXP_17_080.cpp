f83afb5227ca83647f2f1527dd14d39e4056386a
Mahil Anthony <mahilsamson2016@gmail.com>
2024-10-15 17:33:57 +0530

    Create EXP_17_080.cpp

---
+++ EXP_17_080.cpp
@@ -0,0 +1,72 @@
+//Experiment 16 - To study and implement about exception handling.
+//Name - Mahil Anthony
+//PRN -  23070123080
+//Batch -  ENTC B1
+
+//Program 1
+#include<iostream>
+using namespace std;
+class Link{
+    public:
+    int data ;
+    Link* next;
+    Link(int num){
+        data = num;
+        next = NULL;
+    }
+};
+void insert_head(Link* &head, int data){
+    Link* new_node = new Link(data);
+    new_node -> next= head;
+    head = new_node;
+}
+void disp(Link*head){
+    Link*temp = head;
+    while (temp!= NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+
+    }
+    cout<<"NULL"<<endl;
+}
+
+int main(){
+    Link*head = NULL;
+    insert_head(head, 30);
+    disp(head);
+    insert_head(head, 32);
+    disp(head);
+    insert_head(head,35);
+    disp(head);
+    
+}
+
+//Output
+/*
+30->NULL
+32->30->NULL
+35->32->30->NULL
+*/
+
+//Program 2
+#include<iostream>
+using namespace std;
+class Link{
+    public:
+    int data ;
+    Link* next;
+    Link(int num){
+        data = num;
+        next = NULL;
+    }
+};
+int main(){
+    Link* l1 = new Link(6);
+    cout << l1->data<<"   "<<l1->next;
+
+}
+
+//Output
+/*
+6   0
+*/
